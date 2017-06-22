57b304d89ebe7de0f1214abd177bb7ec3adb16cf
Jon <jon@jgubman.com>
2017-06-22 16:46:07 -0700

    inline the fonts

--- dist/videojs.theaterMode.js
+++ dist/videojs.theaterMode.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports={
   "name": "videojs-theater-mode",
-  "version": "0.0.1",
+  "version": "0.1.0",
   "description": "Adds a class the video.js container that can be used to put your video into \"theater mode\"",
   "main": "dist/videojs-theater-mode.cjs.js",
   "module": "dist/videojs-theater-mode.es.js",
